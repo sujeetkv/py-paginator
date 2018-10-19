641f693c3115fca28096a7ae4dcd9a109b418bef
Sujeet Kumar <sujeetkv90@gmail.com>
2018-10-19 15:13:36 +0530

    fixed circular reference issue

--- py_paginator.py
+++ py_paginator.py
@@ -68,13 +68,13 @@ class Paginator(object):
     @property
     def has_prev(self):
         """Check if has previous page"""
-        return (self.curr_page > self.first_page)
+        return (self._curr_page > self.first_page)
 
 
     @property
     def has_next(self):
         """Check if has next page"""
-        return (self.curr_page < self.last_page)
+        return (self._curr_page < self.last_page)
 
 
     def get_pager(self):
@@ -86,7 +86,7 @@ class Paginator(object):
         if self.has_pages:
             pager['first'] = self.first_page
             pager['prev'] = self.prev_page if self.has_prev else None
-            pager['curr'] = self.curr_page
+            pager['curr'] = self._curr_page
             pager['next'] = self.next_page if self.has_next else None
             pager['last'] = self.last_page
         return pager
@@ -115,17 +115,17 @@ class Paginator(object):
                 self._append_range_pages(1, self.last_page + 1)
             elif self.last_page >= (5 + adj_count):# enough pages to hide some
                 # close to beggining; only hide later pages
-                if self.curr_page < (1 + adj_count):
+                if self._curr_page < (1 + adj_count):
                     self._append_range_pages(1, 4 + adj_count)
                     self.page_list.append(('ellip', '...'))
                     self.page_list.append(('page', second_last))
                     self.page_list.append(('page', self.last_page))
                 # in middle; hide some front and some back
-                elif (self.last_page - adj_count) > self.curr_page and self.curr_page > adj_count:
+                elif (self.last_page - adj_count) > self._curr_page and self._curr_page > adj_count:
                     self.page_list.append(('page', 1))
                     self.page_list.append(('page', 2))
                     self.page_list.append(('ellip', '...'))
-                    self._append_range_pages(self.curr_page - adjacents, self.curr_page + adjacents + 1)
+                    self._append_range_pages(self._curr_page - adjacents, self._curr_page + adjacents + 1)
                     self.page_list.append(('ellip', '...'))
                     self.page_list.append(('page', second_last))
                     self.page_list.append(('page', self.last_page))
@@ -147,17 +147,17 @@ class Paginator(object):
 
     def _process_page_numbers(self):
         self.last_page = int(math.ceil(float(self.total_items) / float(self.item_limit)))
-        if self.curr_page < 1 or self.curr_page > self.last_page:
-           self.curr_page = 1
-        self.prev_page = self.curr_page - 1
-        self.next_page = self.curr_page + 1
+        if self._curr_page < 1 or self._curr_page > self.last_page:
+           self._curr_page = 1
+        self.prev_page = self._curr_page - 1
+        self.next_page = self._curr_page + 1
         self.item_offset = self.prev_page * self.item_limit
         self.page_list = []
 
 
     def _append_range_pages(self, start, stop):
         for counter in range(start, stop):
-            if counter == self.curr_page:
+            if counter == self._curr_page:
                 self.page_list.append(('curr', counter))
             else:
                 self.page_list.append(('page', counter))
